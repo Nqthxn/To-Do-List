ea38aa3aee364717ec60f6ca34ef107e48d19e9b
Nathan Lam <nathanl1605@gmail.com>
2024-08-02 13:21:06 +0100

    Add / View tasks functionality

--- Main.java
+++ Main.java
@@ -33,6 +33,7 @@ public class Main {
         System.out.print("Enter your choice : ");
         int choice = input.nextInt();
         input.nextLine();
+        System.out.println();
 
         switch(choice){
             case 1:
@@ -41,18 +42,47 @@ public class Main {
             case 2:
                 markComplete(input);
                 break;
+            case 3:
+                deleteTask(input);
+                break;
+            case 4:
+                viewTasks();
+                break;
+            case 5:
+                System.out.println("Exiting.....");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid Choice. Please Try Again.\n\n");
+                displayOptions();
         }
     }
     public static void addTask(Scanner input){
+        System.out.print("Enter task description : ");
+        String description = input.nextLine();
+
+        System.out.print("Enter due date (DD/MM/YY) : ");
+        String date = input.nextLine();
+
+        System.out.print("Enter priority (High / Medium / Low) : ");
+        String prior = input.nextLine();
+
+        taskList.add(new Tasks(description, date, prior));
+        System.out.println("Task added successfully!\n");
+        displayOptions();
 
     }
+    
     public static void markComplete(Scanner input){
 
     }
-    public static void deleteTask(){
+    public static void deleteTask(Scanner input){
 
     }
     public static void viewTasks(){
-
+        System.out.println("========== TASKS ==========");
+        for(int j = 0; j < taskList.size(); j++){
+            System.out.println(taskList.get(j));
+        }
     }
 }
